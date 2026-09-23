514255949caf5587b09c9827cdcec6ce2fba4a3d
bonum119 <133674216+bonum119@users.noreply.github.com>
2026-09-23 20:02:24 +0200

    Borrar ej antiguo

--- Ejercicio_Gasolinera_Arreglado/src/PagoRepositorio.java
+++
@@ -1,9 +0,0 @@
-import java.time.LocalDate;
-import java.util.List;
-
-public interface PagoRepositorio {
-
-    Pagos guardar(int idCliente, LocalDate fecha, double importe, double litros, String combustible);
-
-    List<Pagos> listarTodos();
-}
